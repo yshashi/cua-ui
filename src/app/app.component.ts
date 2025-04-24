0b414cbb67570838202333977f28a4c572ba51a2
Sashikumar <yshashi30@gmail.com>
2025-04-24 11:05:22 +0800

    feat: Add Tailwind CSS and configure PostCSS for styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
